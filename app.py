e318ec5daec65f05afc5ec4bc221a545b958fa6d
tal-3370 <44543761+tal-3370@users.noreply.github.com>
2023-02-03 09:51:45 +0200

    added commnts and fixed bug

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from setup_db import execute_query
 
 @app.route('/register/<student_id>/<course_id>')
 def register(student_id, course_id):
-    execute_query("INSERT...")
-    redirect(url_for('registrations', student_id=student_id))
+    """
+    This endpoint inserts a student into students_courses table so student_id is registered to 
+    course_id. Then show all courses for this student.
+    """
+    execute_query(f"INSERT INTO students_courses (student_id, course_id) VALUES ('{student_id}', '{course_id}')")
+    return redirect(url_for('registrations', student_id=student_id))
 
 @app.route('/registrations/<student_id>')
 def registrations(student_id):
+    """
+    1. Get course IDs for this student using student_courses
+    2. Get course names using course ids
+    """
     course_ids=execute_query(f"SELECT course_id FROM students_courses WHERE student_id={student_id}")
     clean_ids=[ c[0] for c in course_ids ]
     course_names=[]
@@ -18,6 +26,6 @@ def registrations(student_id):
     return render_template("registrations.html", student_name=student_name, course_names=course_names)
 
 
-# cha
+
 
 
